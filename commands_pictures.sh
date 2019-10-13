9f94e7429b796c5c346aae259317eaa620f75306
xexpanderx <andrei.alexsson@gmail.com>
2019-10-14 00:50:29 +0200

    Kill dropbox when finding new images

--- commands_pictures.sh
+++ commands_pictures.sh
@@ -4,12 +4,18 @@ cd /home/alexsson/Dropbox/ConkyPi/Pictures/
 not_empty_jpg=`find . -name "*.jpg"`
 if [ ! -z "$not_empty_jpg" ]
 then
-  mogrify -format png *.jpg && rm *.jpg
+  killall dropbox
+  mogrify -format png *.jpg
+  rm *.jpg
 fi
 
 new_pics=`find . -not -path '*/\.*' -type f ! -name "*resized.png"`
 if [ ! -z "$new_pics" ]
 then
+  killall dropbox
   for image in $(/usr/bin/ls | grep -v '\_resized.png'); do value=`identify $image | awk '{print $3}' | sed 's/x/\n/g' | sort -n | head -1` ; scale=$(expr $value / 480) ; if [ $scale -eq 0 ] ; then scale=1 ; else : ; fi ;scale=$(expr 100 / $scale) ; mogrify -resize ${scale}% $image ; done
-  for file in $(/usr/bin/ls | grep -v '\_resized.png'); do mv $file ${file//.png/}_resized.png ; done && cp *_resized.png .show_pics/
+  for file in $(/usr/bin/ls | grep -v '\_resized.png'); do mv $file ${file//.png/}_resized.png ; done
+  cp *_resized.png .show_pics/
+  sleep 10
+  dropboxd &
 fi
