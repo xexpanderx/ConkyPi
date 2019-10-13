f0c31766ad6960cdc1eaced01729de2535dde071
xexpanderx <andrei.alexsson@gmail.com>
2019-10-14 00:39:37 +0200

    Fix division by zero

--- commands_pictures.sh
+++ commands_pictures.sh
@@ -10,6 +10,6 @@ fi
 new_pics=`find . -not -path '*/\.*' -type f ! -name "*resized.png"`
 if [ ! -z "$new_pics" ]
 then
-  for image in $(/usr/bin/ls | grep -v '\_resized.png'); do value=`identify $image | awk '{print $3}' | sed 's/x/\n/g' | sort -n | head -1` ; scale=$(expr $value / 480) ; scale=$(expr 100 / $scale) ; mogrify -resize ${scale}% $image ; done
+  for image in $(/usr/bin/ls | grep -v '\_resized.png'); do value=`identify $image | awk '{print $3}' | sed 's/x/\n/g' | sort -n | head -1` ; scale=$(expr $value / 480) ; if [ $scale -eq 0 ] ; then scale=1 ; else : ; fi ;scale=$(expr 100 / $scale) ; mogrify -resize ${scale}% $image ; done
   for file in $(/usr/bin/ls | grep -v '\_resized.png'); do mv $file ${file//.png/}_resized.png ; done && cp *_resized.png .show_pics/
 fi
